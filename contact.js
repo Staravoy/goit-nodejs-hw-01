ea2d713f756c11f9da90bfadbbeb7ea8a65556ba
Staravoy <85824511+Staravoy@users.noreply.github.com>
2023-03-18 01:33:25 +0200

    hometask

---
+++ contact.js
@@ -0,0 +1,59 @@
+const fs = require("fs").promises;
+const path = require("node:path");
+const { v4 } = require("uuid");
+
+const contactsPath = path.resolve(__dirname, "./db/contact.js");
+async function listContacts() {
+  try {
+    const data = await fs.readFile(contactsPath);
+    const contacts = JSON.parse(data);
+    return contacts;
+  } catch (error) {
+    console.log(error.message);
+  }
+}
+async function getContactById(contactId) {
+  try {
+    const contacts = await listContacts();
+    const idRes = contacts.find(({ id }) => id === contactId);
+    return idRes;
+  } catch (error) {
+    console.log(error.message);
+  }
+}
+async function updateFile(instance) {
+  try {
+    fs.writeFile(contactsPath, JSON.stringify(instance, null, 2));
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+async function removeContact(contactId) {
+  try {
+    const allContacts = await listContacts();
+    const changedCollection = allContacts.filter(({ id }) => id !== contactId);
+    updateFile(changedCollection);
+    return allContacts.filter(({ id }) => id === contactId);
+  } catch (error) {
+    console.log(error);
+  }
+}
+async function addContact(name, email, phone) {
+  try {
+    const newContact = { id: v4(), name: name, email: email, phone: phone };
+    const allContacts = await listContacts();
+    const changed = [...allContacts, newContact];
+    updateFile(changed);
+    return newContact;
+  } catch (error) {
+    console.log(error);
+  }
+}
+module.exports = {
+  listContacts,
+  getContactById,
+  removeContact,
+  addContact,
+  updateFile,
+};
